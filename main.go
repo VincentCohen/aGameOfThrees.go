16cf7c800cec714907d2c945f2eb46197c1bd377
Vincent <vincent@getseoshop.com>
2018-01-06 20:06:35 +0100

    Game of threes finished!

--- main.go
+++ main.go
@@ -12,5 +12,51 @@ func main() {
 	var num int
 	fmt.Scanf("%d", &num)
 
-	fmt.Println("You selected:", num)
+	calc(num)
+}
+
+func calc(number int) {
+	if number == 1 {
+		fmt.Println(number, "END")
+		return
+	}
+
+	if isDividable(number) {
+
+		fmt.Println(number, "0")
+
+		number = divide(number)
+
+		calc(number)
+	} else {
+		if isDividable(number - 1) {
+			fmt.Println(number, "-1")
+
+			number = divide(minus(number))
+
+			calc(number)
+		} else {
+			fmt.Println(number, "+1")
+
+			number = divide(add(number))
+
+			calc(number)
+		}
+	}
+}
+
+func isDividable(number int) bool {
+	return number%3 == 0
+}
+
+func divide(number int) int {
+	return number / 3
+}
+
+func minus(number int) int {
+	return number - 1
+}
+
+func add(number int) int {
+	return number + 1
 }
